26880edda5bda974f24abf0e03762e8052122526
Kristóf Hetényi <kristofhetenyi228@gmail.com>
2021-02-11 00:51:34 +0100

    Script to mount installing dir
    This script can mount the installing directory from the main server.

---
+++ linux/mnts/mntinstall.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+cwd=$(dirname $(readlink -f $0)) # current wrinting dir
+iserver="//192.168.1.15/install"
+user=$(whoami)
+
+if [ -d "/mnt/shared" ]; then
+	mkdir "/mnt/shared"
+	if [ -d "/mnt/shared/install" ]; then
+		mkdir "/mnt/shared/install"
+	fi
+fi
+
+sudo apt-get update # update repos
+sudo apt-get install cifs-utils -y # download cifs
+
+mnt.cifs $iserver "/mnt/shared/install" -o user=$user
